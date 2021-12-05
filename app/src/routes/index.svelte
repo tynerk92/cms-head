3794812a7f1d43521d6b575dea79dfaf881f11ef
Kylon Tyner <tynerk92@gmail.com>
2021-12-05 12:59:11 -0500

    Added netlify identity and CMS

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,2 +1,16 @@
+<script context="module">
+
+</script>
+
+<script>
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    const cms = await import('netlify-cms')
+
+    console.log(cms)
+  })
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
