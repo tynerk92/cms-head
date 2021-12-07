70f81bf8d479345f479ec9031fe49fc054173b54
Kylon Tyner <tynerk92@gmail.com>
2021-12-06 20:07:28 -0500

    Baseline

---
+++ src/routes/admin/index.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    const CMS = await import('netlify-cms')
+  })
+</script>
+
+<svelte:head>
+  <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+</svelte:head>
---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>Index</h1>
+
+<style>
+
+</style>
