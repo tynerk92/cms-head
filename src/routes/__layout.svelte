02505408045a680ecb4658641d59d2cec0ec62aa
Kylon Tyner <tynerk92@gmail.com>
2021-12-08 17:06:36 -0500

    Adding link to testing post to allow route activation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,2 +1,19 @@
+<script>
+
+</script>
+
 <h1>Layout</h1>
 <slot></slot>
+
+<div class="sr-only">
+  <a rel="prefetch" href="posts/testing">Testing</a>
+</div>
+
+<style>
+  .sr-only {
+    position: absolute;
+    top: -1000px;
+    left: -1000px;
+    visibility: hidden;
+  }
+</style>
