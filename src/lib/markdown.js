b4240fa757d1323ab6a7e9d9600cbf39102492e2
Kylon Tyner <tynerk92@gmail.com>
2021-12-08 18:59:04 -0500

    Trying to print out current directory contents

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -4,9 +4,14 @@ export async function process(filename) {
   const METADATA_REGEX = /---((.|\r\n)*)---/g
   const METADATA_TAGS_REGEX = /---(\r\n)?/g
 
-  const md = fs.readFileSync(filename, 'utf8')
+  const files = fs.readdirSync('.')
+  console.log(files)
+
+  const md = fs.readFileSync(files.join(', '))
+  // const md = fs.readFileSync(filename, 'utf8')
 
   const metaTag = md.match(METADATA_REGEX)[0]
   const metaData = metaTag.replace(METADATA_TAGS_REGEX, '')
-  return md.replace(metaTag, '').trim()
+  // return md.replace(metaTag, '').trim()
+  return files
 }
