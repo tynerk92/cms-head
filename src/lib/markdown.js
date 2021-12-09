2a6b8f2639e51547e80a4eaa1cfd1d699ba251f6
Kylon Tyner <tynerk92@gmail.com>
2021-12-08 19:27:05 -0500

    Attempting fs read again

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -4,14 +4,11 @@ export async function process(filename) {
   const METADATA_REGEX = /---((.|\r\n)*)---/g
   const METADATA_TAGS_REGEX = /---(\r\n)?/g
 
-  const files = fs.readdirSync('..')
-  console.log(files)
-
-  const md = fs.readFileSync(files.join(', '))
+  const md = fs.readFileSync('./posts/testing.md')
   // const md = fs.readFileSync(filename, 'utf8')
 
   const metaTag = md.match(METADATA_REGEX)[0]
   const metaData = metaTag.replace(METADATA_TAGS_REGEX, '')
-  // return md.replace(metaTag, '').trim()
-  return files
+  return md.replace(metaTag, '').trim()
+  return md
 }
