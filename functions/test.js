05f09a974bbde3fadd7b8d49f34be817ab7497f6
Kylon Tyner <tynerk92@gmail.com>
2021-12-08 21:55:58 -0500

    Trying again

--- functions/test.js
+++ functions/test.js
@@ -1,9 +1,7 @@
 module.exports.handler = async (event, context) => {
   const { slug } = event.queryStringParameters
-  console.log("Running function - slug: ", slug)
   const resourceURL = `./posts/${slug}.md`
   const md = require(resourceURL)
-  console.log("Found MD: ", md)
 
   return {
     statusCode: 200,
