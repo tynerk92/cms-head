872be4f4690d7e061fc71533a41372242a9fa3b2
Kylon Tyner <tynerk92@gmail.com>
2021-12-08 21:30:38 -0500

    Trying again

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,7 @@
 module.exports.handler = async (event, context) => {
   const { slug } = event.queryStringParameters
   console.log("Running function - slug: ", slug)
-  const md = await import(`./_posts/${slug}.md?raw`)
+  const md = require(`./_posts/${slug}.md?raw`)
   console.log("Found MD: ", md)
 
   return {
